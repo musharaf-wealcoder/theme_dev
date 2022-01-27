b71a4b9495dc777a3393c2262f5af4f83719ce2a
Musharaf Hossain <musharaf.wealcoder@gmail.com>
2022-01-27 17:41:48 +0600

    this is last

--- index.php
+++ index.php
@@ -78,6 +78,11 @@
                 &copy; LWHH - All Rights Reserved
             </div>
         </div>
+    </div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
         
 <!--       from mito vay   -->
         <div class="row">
@@ -85,7 +90,21 @@
                 &copy; LWHH - All Rights Reserved
             </div>
         </div>
+
+
+        <div class="row">
+            <div class="col-md-12">
+                &copy; LWHH - All Rights Reserved
+            </div>
+        </div><div class="row">
+            <div class="col-md-12">
+                &copy; LWHH - All Rights Reserved
+            </div>
+        </div>
     </div>
+
+
+
 </div>
 <?php wp_footer(); ?>
 </body>
